67c262c872efa149dc9201ab85eb35462ec2072f
Scott <zwb_scott@icloud.com>
2016-06-30 23:42:44 +0800

    redux

--- react-redux/public/app/actions/index.js
+++ react-redux/public/app/actions/index.js
@@ -1,6 +1,8 @@
 /**
 *
-* Action向store派发指令，action 函数会返回一个带有 type 属性的 Javascript Plain Object，store将会根据不同的action.type来执行相应的方法。addItem函数的异步操作我使用了一点小技巧，使用redux-thunk中间件去改变dispatch，dispatch是在View层中用bindActionCreators绑定的。
+* Action向store派发指令，action 函数会返回一个带有 type 属性的 Javascript Plain Object，
+ * store将会根据不同的action.type来执行相应的方法。addItem函数的异步操作我使用了一点小技巧，
+ * 使用redux-thunk中间件去改变dispatch，dispatch是在View层中用bindActionCreators绑定的。
 * 使用这个改变的dispatch我们可以向store发送异步的指令。
 * 比如说，可以在action中放入向服务端的请求(ajax)，也强烈推荐这样去做。
 *
