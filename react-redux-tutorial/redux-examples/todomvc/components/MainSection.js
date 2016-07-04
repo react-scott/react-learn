68266184f30ccfcaf26fd5ac33936ecf5093e00f
Scott <zwb_scott@icloud.com>
2016-07-05 00:31:27 +0800

    annotation

--- react-redux-tutorial/redux-examples/todomvc/components/MainSection.js
+++ react-redux-tutorial/redux-examples/todomvc/components/MainSection.js
@@ -16,10 +16,20 @@ class MainSection extends Component {
   }
 
   handleClearCompleted() {
+    /**
+     * 遍历数组，在每一项元素后面触发一个回调函数，通过判断，返回一个布尔值。
+     * some()是只要有一个满足判断，就返回true，every()是只要有一项不满足判断，就返回false。
+     *
+     * 遍历任务数组，有一个任务的属性completed为true，就返回true。
+     *
+     * @type {boolean}
+     */
     const atLeastOneCompleted = this.props.todos.some(todo => todo.completed)
+
     if (atLeastOneCompleted) {
       this.props.actions.clearCompleted()
     }
+
   }
 
   handleShow(filter) {
@@ -59,16 +69,40 @@ class MainSection extends Component {
     const { filter } = this.state
 
     const filteredTodos = todos.filter(TODO_FILTERS[filter])
+
+    /**
+     * 遍历数组，在每一项元素后面触发一个回调函数，经过计算返回一个累加的值。
+     */
     const completedCount = todos.reduce((count, todo) =>
       todo.completed ? count + 1 : count,
       0
     )
+
+      /**
+       * todos是个数组，reduce()的第一个参数是个箭头语法，也就是个回调函数，
+       *
+       * 这个回调函数的第一个参数是上一个返回值（但是这里被初始化为0）,第二个参数是当前元素的值。
+       *
+       * reduce()的第二个参数是个初始化值（不必需），初始化了上一个元素的值（这里是count）
+
+       遍历数组todos的第一个值的时候，count为0，todo是todos的第一项，返回值加一或者不变。
+
+       （条件 ? 结果1 : 结果2三元运算）
+
+       遍历数组todos的第二个值的时候，count为上一个返回值，todo是todos的第二项，返回值加一或者不变。
+
+       遍历结束后，即可得到数组中，completed属性为true的个数，也就是已完成的任务的个数。
+       */
 
     return (
       <section className="main">
         {this.renderToggleAll(completedCount)}
         <ul className="todo-list">
           {filteredTodos.map(todo =>
+              /**
+               * 展开actions的每一个属性到组件中，最后在props上可以获取到。
+               * @param todo
+               */
             <TodoItem key={todo.id} todo={todo} {...actions} />
           )}
         </ul>
--- react-redux-tutorial/redux-examples/todomvc/reducers/todos.js
+++ react-redux-tutorial/redux-examples/todomvc/reducers/todos.js
@@ -18,13 +18,39 @@ export default function todos(state = initialState, action) {
           text: action.text
         }, 
         ...state
+        /**
+         * ...展开属性
+         *
+         * 展开state数组的每一项到当前的数组
+         * 
+         */
       ]
 
+      /**
+       * 遍历数组，在每一项元素后面触发一个回调函数，通过判断，保留或移除当前项，最后返回一个新数组。
+
+         顾名思义就是过滤。
+
+       state是个任务数组，filter()里面只有一个参数，就是个箭头函数，该函数只有一个参数是todo，
+       也就是数组的每一项元素，箭头后面那个判断语句，如果返回true则保留当前项，反之移除当前项。
+
+       该代码段的作用是，过滤掉任务数组中，id与指定id相同的任务。返回一个新的任务数组。
+       */
     case DELETE_TODO:
       return state.filter(todo =>
         todo.id !== action.id
       )
 
+      /**
+       * 遍历数组，在每一项元素后面触发一个回调函数，通过计算，返回一个新的当前项，最后返回一个新数组。
+       *
+       * 箭头后面的值是个三元运算符，也就是return的新元素。如果id匹配，则通过Object.assign()合并一个新的属性，
+       * 也就是给todo添加或者重写一个text属性，属性值为action.text。
+         Object.assign()第一个参数是target，就是目标，第二个第三个以及后面的参数都是source，也就是拷贝的源，
+         是不是很像jquery插件中的extend？
+
+          这个代码的作用是给数组中指定的任务更新text属性。
+       */
     case EDIT_TODO:
       return state.map(todo =>
         todo.id === action.id ?
@@ -40,7 +66,12 @@ export default function todos(state = initialState, action) {
       )
 
     case COMPLETE_ALL:
+      /**
+       * 遍历任务数组，每一项任务的completed属性均为true时候，返回true。
+       * @type {boolean}
+         */
       const areAllMarked = state.every(todo => todo.completed)
+
       return state.map(todo => Object.assign({}, todo, {
         completed: !areAllMarked
       }))
