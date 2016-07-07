73e72d97269fc4cc9fbcf69b76a827458120e76a
Scott <zwb_scott@icloud.com>
2016-07-08 00:32:16 +0800

    01-setting-up

---
+++ react-redux-tutorial/01-setting-up/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by scott on 7/8/16.
+ */
+
+import React from 'react'
+
+import { render } from 'react-dom'
+
+import App from './modules/App'
+
+render(
+    <App/>,
+    document.getElementById('app')
+)
+
+
